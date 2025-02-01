0be67917e661a59cc6930ef0c802f44669b17603
Kevin Dragan <kevin.dragan@outlook.comm>
2025-02-01 17:49:45 -0600

    fully converted now with uv. Next steps, explore improving to usnig actual pdf docs for rag, and moving to supabase to enble both vectors and metadata database searches.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.api.v1.endpoints.chat import router as chat_router
+from src.app.api.v1.endpoints.chat import router as chat_router
 
 app = FastAPI(
     title="Chat API",
@@ -13,4 +13,4 @@ app.include_router(chat_router, prefix="/api/v1")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
